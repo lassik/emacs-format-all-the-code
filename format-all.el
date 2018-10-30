82d223a1bc71eda859dca8eb756efdb06571759f
Lassi Kortela <lassi@lassi.io>
2018-10-30 17:59:38 +0200

    Update format-all-mode documentation
    Also clarify the code example

--- format-all.el
+++ format-all.el
@@ -568,31 +568,34 @@ changes to the code, point is placed at the first change."
   "Toggle automatic source code formatting before save.
 
 When the Format-All minor mode is enabled, `format-all-buffer' is
-automatically called each time before you save the buffer.
+automatically called to format your code each time before you
+save the buffer.
+
+The mode is buffer-local and needs to be enabled separately each
+time a file is visited.  You may want to use `add-hook' to add a
+function to your personal `after-change-major-mode-hook' in your
+`user-init-file' to enable the mode based on the buffer's
+`major-mode' and some `buffer-file-name' patterns. For example:
+
+    (defvar my-auto-format-modes '(js-mode python-mode))
+    (defvar my-auto-format-dirs '(\"foo\" \"bar\"))
+
+    (defun my-auto-format-buffer-p ()
+      (and (member major-mode my-auto-format-modes)
+           (buffer-file-name)
+           (save-match-data
+             (let ((dir (file-name-directory (buffer-file-name))))
+               (cl-some (lambda (regexp) (string-match regexp dir))
+                        my-auto-format-dirs)))))
+
+    (defun my-after-change-major-mode ()
+      (format-all-mode (if (my-auto-format-buffer-p) 1 0)))
+
+    (add-hook 'after-change-major-mode-hook 'my-after-change-major-mode)
 
 When called from Lisp, the mode is toggled if ARG is ‘toggle’,
 disabled if ARG is a negative integer or zero, and enabled
-otherwise.
-
-The mode is buffer-local and needs to be enabled separately each
-time a file is visited or a temporary buffer is created.
-
-You may want to use `add-hook' to add a function to your personal
-`after-change-major-mode-hook' in your `user-init-file' to enable
-the mode based on the buffer's `major-mode' and some
-`buffer-file-name' patterns. For example:
-
-    (defun my-after-change-major-mode ()
-      (format-all-mode
-       (if (and (buffer-file-name)
-                (save-match-data
-                  (let ((dir (file-name-directory (buffer-file-name))))
-                    (or (string-match \"foo\" dir)
-                        (string-match \"bar\" dir))))
-                (member major-mode '(js-mode python-mode)))
-           1 0)))
-
-    (add-hook 'after-change-major-mode-hook 'my-after-change-major-mode)"
+otherwise."
   :lighter " Format-All"
   :global nil
   (if format-all-mode
