9b8c6be199f598d6f636d2b2cfea76b628ac17e9
Lassi Kortela <lassi@lassi.io>
2020-12-11 22:04:54 +0200

    Upgrade language-id to 0.9

--- format-all.el
+++ format-all.el
@@ -3,7 +3,7 @@
 ;; Author: Lassi Kortela <lassi@lassi.io>
 ;; URL: https://github.com/lassik/emacs-format-all-the-code
 ;; Version: 0.3.0
-;; Package-Requires: ((emacs "24.3") (language-id "0.8.1"))
+;; Package-Requires: ((emacs "24.3") (language-id "0.9"))
 ;; Keywords: languages util
 ;; SPDX-License-Identifier: MIT
 ;;
