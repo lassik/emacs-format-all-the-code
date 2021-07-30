ea4ebbaba7c97c11ad276348c0a1c49987f47c27
Lassi Kortela <lassi@lassi.io>
2021-07-30 13:53:11 +0300

    Add admin check script
    - Checks that the formatter definitions are in alphabetical order.
    - Prints how many formatters we have.
    
    It would be useful to check the language and formatter listings in the
    README and Commentary as well.

---
+++ admin/check.el
@@ -0,0 +1,18 @@
+(with-temp-buffer
+  (insert-file-contents "format-all.el")
+  (let (forms)
+    (condition-case () (while t (push (read (current-buffer)) forms))
+      (end-of-file (setq forms (reverse forms))))
+    (let* ((formatter-names
+            (reverse
+             (cl-reduce
+              (lambda (names form)
+                (if (and (consp form)
+                         (eql 'define-format-all-formatter (cl-first form)))
+                    (cons (cl-second form) names)
+                  names))
+              forms :initial-value '())))
+           (sorted-names (sort (cl-copy-list formatter-names) 'string<)))
+      (or (and (equal formatter-names sorted-names)
+               (message "We have %d formatters" (length formatter-names)))
+          (error "Formatter names are not in alphabetical order")))))
